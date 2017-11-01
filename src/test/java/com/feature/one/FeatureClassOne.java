66a8e72486cfa77b0a1c681f94bed3738b7430e8
Tata <Praveen.Tata@McKesson.com>
2017-11-01 15:48:40 -0400

    Adding FeatureClassSix

---
+++ src/test/java/com/feature/one/FeatureClassOne.java
@@ -0,0 +1,12 @@
+package com.feature.one;
+
+import org.testng.annotations.Test;
+
+public class FeatureClassOne {
+
+	@Test
+	public void featureClassOneMethod()
+	{
+		System.out.println("Feature Class One Method.");
+	}
+}
---
+++ src/test/java/com/feature/one/FeatureClassThree.java
@@ -0,0 +1,13 @@
+package com.feature.one;
+
+import org.testng.annotations.Test;
+
+public class FeatureClassThree {
+	
+	@Test
+	public void featureClassThreeMethod()
+	{
+		System.out.println("Feature Class Three Method.");
+	}
+
+}
