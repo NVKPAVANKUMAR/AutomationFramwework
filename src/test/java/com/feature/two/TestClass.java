70ddc78cc7487e55255c6217bb67b933bff683b1
Tata <Praveen.Tata@McKesson.com>
2017-11-01 16:05:42 -0400

    TestClass

---
+++ src/test/java/com/feature/two/TestClass.java
@@ -0,0 +1,13 @@
+package com.feature.two;
+
+import org.testng.annotations.Test;
+
+public class TestClass {
+	
+	@Test
+	public void testClassMethod()
+	{
+		System.out.println("Test Class Method.");
+	}
+
+}
