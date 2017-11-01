3682de3b61d3a2e4c99b77dfc4a941d1e2528f16
Tata <Praveen.Tata@McKesson.com>
2017-11-01 14:56:36 -0400

    Branch Class

---
+++ src/test/java/com/test/feature/BranchClass.java
@@ -0,0 +1,13 @@
+package com.test.feature;
+
+import org.testng.annotations.Test;
+
+public class BranchClass {
+	
+	@Test
+	public void branchClassMethod()
+	{
+		System.out.println("Branch Class Method.");
+	}
+
+}
