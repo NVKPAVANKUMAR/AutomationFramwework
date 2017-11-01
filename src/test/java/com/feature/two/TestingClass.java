35333babb4b7cf077ff9b1b49618c46513e1a088
Tata <Praveen.Tata@McKesson.com>
2017-11-01 16:11:51 -0400

    Added TestingClass

---
+++ src/test/java/com/feature/two/TestingClass.java
@@ -0,0 +1,13 @@
+package com.feature.two;
+
+import org.testng.annotations.Test;
+
+public class TestingClass {
+	
+	@Test
+	public void testingClassMethod()
+	{
+		System.out.println("Testing Class Method");
+	}
+
+}
